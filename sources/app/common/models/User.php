7fa0abbc4ac4ae9893969110613807112e87f4ed
Juriy Panasevich <elfelrandcor@gmail.com>
2018-09-14 14:14:01 +0300

    Поиск неподтвержденного пользователем приза

--- sources/app/common/models/User.php
+++ sources/app/common/models/User.php
@@ -187,4 +187,9 @@ class User extends ActiveRecord implements IdentityInterface
     {
         $this->password_reset_token = null;
     }
+
+    public function getActivePrize()
+    {
+        return Prize::find()->byUser($this)->reserved()->one();
+    }
 }
--- sources/app/common/models/prize/ActiveQuery.php
+++ sources/app/common/models/prize/ActiveQuery.php
@@ -6,6 +6,7 @@
 namespace common\models\prize;
 
 
+use common\models\Prize;
 use common\models\User;
 
 class ActiveQuery extends \yii\db\ActiveQuery
@@ -14,5 +15,10 @@ class ActiveQuery extends \yii\db\ActiveQuery
     public function byUser(User $user)
     {
         return $this->andWhere(['user_id' => $user->id]);
+    }
+
+    public function reserved()
+    {
+        return $this->andWhere(['status' => Prize::STATUS_RESERVED]);
     }
 }
