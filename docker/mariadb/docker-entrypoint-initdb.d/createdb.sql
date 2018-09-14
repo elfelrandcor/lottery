cf96f25a72abeaf878cc4558ebe5bb2562ecba26
Juriy Panasevich <elfelrandcor@gmail.com>
2018-09-14 11:33:20 +0300

    Инициализация приложения. База данных для тестов

--- docker/mariadb/docker-entrypoint-initdb.d/createdb.sql
+++ docker/mariadb/docker-entrypoint-initdb.d/createdb.sql
@@ -1,4 +1,7 @@
 CREATE DATABASE IF NOT EXISTS `yii2advanced` COLLATE 'utf8mb4_general_ci';
 GRANT ALL ON `yii2advanced`.* TO 'root'@'%';
 
+CREATE DATABASE IF NOT EXISTS `yii2advanced_test` COLLATE 'utf8mb4_general_ci';
+GRANT ALL ON `yii2advanced_test`.* TO 'root'@'%';
+
 FLUSH PRIVILEGES ;
