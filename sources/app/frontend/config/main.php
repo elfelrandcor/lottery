d1533cabf888087ef79a0fcd40f291055a16f6e5
Juriy Panasevich <elfelrandcor@gmail.com>
2018-09-14 15:31:41 +0300

    Рефакторинг

--- sources/app/frontend/config/main.php
+++ sources/app/frontend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-frontend',
+    'name' => 'Lottery',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
